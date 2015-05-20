23f8ec332174470f9add59026be369cf3633e180
Victor Queiroz <victorcqueirozg@gmail.com>
2015-05-20 02:09:13 -0300

    alot of scss improvements

--- build/mobie.tpl.js
+++ build/mobie.tpl.js
@@ -1,1 +1,1 @@
-angular.module("mobie").run(["$templateCache", function($templateCache) {$templateCache.put("components/modal/modal.html","<mb-modal></mb-modal>");}]);
+angular.module("mobie").run(["$templateCache", function($templateCache) {$templateCache.put("components/modal/modal.html","<div class=\"modal\"></div>");}]);
