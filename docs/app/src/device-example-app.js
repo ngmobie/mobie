9050df2cbd44bf2122cd34f1379b8aab070043e7
Victor Queiroz <victorcqueirozg@gmail.com>
2015-06-02 23:46:17 -0300

    documentation(device-example-app): better examples possibilities

--- docs/app/src/device-example-app.js
+++ docs/app/src/device-example-app.js
@@ -1,16 +1,59 @@
+function getRandomThumbnailUrl() {
+	var keys = _.keys(faker.image);
+
+	var thumbnailUrl = faker.image[keys[Math.floor(Math.random() * keys.length)]]();
+
+	return thumbnailUrl;
+}
+
 function FakerController ($scope, $window) {
 	this.getPersons = function () {
 		var persons = [];
 		for(var i=0; i<100; i++) {
-			persons[i] = {
+			var person = {
 				id: i,
 				name: faker.name.findName(),
-				checked: false
+				checked: false,
+				avatarUrl: faker.image.avatar(),
+				bio: faker.lorem.sentences(8)
 			};
+
+			var posts = [];
+			for(var j=0; j<10; j++) {
+				var post = {
+					title: faker.lorem.sentence(3),
+					authorId: person.id,
+					createdAt: faker.date.past(),
+					updatedAt: faker.date.recent()
+				};
+
+				post.title = _.capitalize(post.title);
+				post.author = person;
+				post.thumbnailUrl = getRandomThumbnailUrl();
+
+				posts[j] = post;
+			}
+
+			person.posts = posts;
+
+			persons[i] = person;
 		}
+
 		return persons;
 	};
-	$scope.persons = this.getPersons();
+	
+	this.persons = $scope.persons = this.getPersons();
+
+	this.getPersonById = function (personId) {
+		var person;
+		this.persons.forEach(function (key) {
+			if(key.id === Number(personId)) {
+				person = key;
+			}
+		});
+		return person;
+	};
+
 	this.checkPerson = function (person) {
 		person.checked = !!!person.checked;
 	};
