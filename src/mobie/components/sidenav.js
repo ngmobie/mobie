9451bed7c6dd49b5785e35231045c9f2f060d476
Victor Queiroz <victorcqueirozg@gmail.com>
2015-06-06 19:36:23 -0300

    chore(mbSidenav): documentation annotations added

--- src/mobie/components/sidenav.js
+++ src/mobie/components/sidenav.js
@@ -81,6 +81,71 @@ function $MbSidenavProvider () {
 /**
  * @ngdoc directive
  * @name mbSidenav
+ *
+ * @example
+ * In this example you can see a active and fully working sidenav strategy, a sidenav is married to the `$componentRegistry` and you can have how many sidenavs you want to. As long as you define the `componentId` attribute in the sidenav element.
+  <example module="exampleApp">
+  	<file name="index.html">
+			<div class="sidenav" mb-sidenav data-component-id="sidenav-left" mb-animation="slide-in-left slide-out-left">
+				<div class="bar bar-header">
+					<h3 class="title">Sidenav Left</h3>
+				</div>
+			</div>
+
+			<div class="sidenav sidenav-right" mb-sidenav data-component-id="sidenav-right" mb-animation="slide-in-right slide-out-right">
+				<div class="bar bar-header">
+					<h3 class="title">Sidenav Right</h3>
+				</div>
+			</div>
+
+			<div class="sidenav sidenav-top" mb-sidenav data-component-id="sidenav-top" mb-animation="slide-in-up slide-out-down">
+				<div class="bar bar-header">
+					<h3 class="title">"Sidenav" Top</h3>
+				</div>
+			</div>
+
+ 			<div ng-controller="BarController as barCtrl">
+				<div class="bar bar-header">
+					<button class="button button-clear icon ion-navicon" ng-click="barCtrl.toggle()"></button>
+					<h3 class="title">Content bar</h3>
+					<button class="button button-clear icon ion-navicon" ng-click="barCtrl.toggleSecond()"></button>
+				</div>
+
+				<div class="row">
+					<div class="col">
+						<p>That is my content</p>
+
+						<p>
+							<button class="button button-block" ng-click="barCtrl.toggleTop()">Toggle Sidenav Top</button>
+						<p>
+					</div>
+				</div>
+			</div>
+  	</file>
+  	<file name="app.js">
+  		angular.module('exampleApp', ['mobie', 'ngAnimate'])
+  		.controller('BarController', ['$mbSidenav', function ($mbSidenav) {
+				this.toggle = function () {
+					return $mbSidenav('sidenav-left').toggle();
+				};
+				this.toggleSecond = function () {
+					return $mbSidenav('sidenav-right').toggle();
+				};
+				this.toggleTop = function () {
+					return $mbSidenav('sidenav-top').toggle();
+				};
+  		}]);
+  	</file>
+  	<file name="app.css">
+			@import url("../../lib/mobie.css");
+			@import url("../../lib/ionicons/css/ionicons.min.css");
+			.sidenav-whatever.mb-visible,
+			.sidenav-whatever.mb-visible-remove,
+			.sidenav-whatever.mb-visible-remove-active {
+			  bottom: 0;
+			}
+  	</file>
+  </example>
  */
 function SidenavDirective () {
 	return {
