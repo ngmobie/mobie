cd72a9289f9fc8e7a5f1338ae7c19de07ccd4cc6
Victor Queiroz <victorcqueirozg@gmail.com>
2015-10-15 13:03:49 -0300

    0.0.78

--- src/mobie/components/backdrop.js
+++ src/mobie/components/backdrop.js
@@ -35,58 +35,6 @@ angular.module('mobie.components.backdrop', [
 			}
 
 			return $animate.enter(this.getElement(), this.containerElement);
-		},
-
-		operate: function(operation) {
-			return this[operation.method].apply(this, operation.args);
-		},
-
-		run: function() {
-			var promises = [];
-
-			angular.forEach(this.queue, function(operation, index) {
-				promises.push(this.operate(operation));
-
-				this.queue.splice(index, 1);
-			}, this);
-
-			this._queuePromise = $q.all(promises).then(function() {
-				this._queuePromise = null;
-
-				if(this.queue.length > 0) {
-					return this.finish();
-				}
-			}.bind(this));
-
-			return this.finish();
-		},
-
-		finish: function() {
-			if(this._queuePromise) {
-				return this._queuePromise;
-			}
-			
-			return this.run();
-		},
-
-		perform: function (method) {
-			var args = mobie.toArray(arguments).slice(1);
-			var operation = {
-				method: method,
-				args: args
-			};
-
-			this.queue.push(operation);
-
-			return this.finish();
-		},
-
-		show: function() {
-			return this.perform('setVisibleState', true);
-		},
-
-		hide: function() {
-			return this.perform('setVisibleState', false);
 		}
 	});
 
--- src/mobie/components/backdrop_test.js
+++ src/mobie/components/backdrop_test.js
@@ -1,35 +1,35 @@
 describe('mobie.components.backdrop', function () {
+	var $mbBackdrop, $animate, $rootScope, backdropEl, MbBackdrop;
+
+	beforeEach(module('ngAnimateMock'));
+	beforeEach(module('mobie.components.backdrop'))
+
+	beforeEach(inject(function (_$mbBackdrop_, _$rootScope_, _$animate_, _MbBackdrop_) {
+		$mbBackdrop = _$mbBackdrop_;
+		$rootScope = _$rootScope_;
+		$animate = _$animate_;
+		MbBackdrop = _MbBackdrop_;
+
+		backdropEl = document.body.querySelector('.backdrop');
+	}));
+	
 	describe('$mbBackdrop', function () {
-		var $mbBackdrop, $animate, $rootScope, backdropEl, MbBackdrop;
-
-		beforeEach(module('ngAnimateMock'));
-		beforeEach(module('mobie.components.backdrop'))
-
-		beforeEach(inject(function (_$mbBackdrop_, _$rootScope_, _$animate_, _MbBackdrop_) {
-			$mbBackdrop = _$mbBackdrop_;
-			$rootScope = _$rootScope_;
-			$animate = _$animate_;
-			MbBackdrop = _MbBackdrop_;
-
-			backdropEl = document.body.querySelector('.backdrop');
-		}))
-
-		it('should show backdrop', function () {
-			assert.equal(false, $mbBackdrop.getVisibleState());
-			
-			$mbBackdrop.show();
-			$animate.flush();
-			
-			assert.equal($mbBackdrop.getElement()[0], backdropEl)
-			assert.ok($mbBackdrop.getVisibleState());
-		});
-
 		it('should have a default id', function () {
 			assert.equal('default-backdrop', $mbBackdrop.getId())
 		});
 
 		describe('show()', function() {
-			it('should queue operations', function() {
+			it('should show backdrop', function () {
+				assert.equal(false, $mbBackdrop.getVisibleState());
+				
+				$mbBackdrop.show();
+				$animate.flush();
+				
+				assert.equal($mbBackdrop.getElement()[0], backdropEl)
+				assert.ok($mbBackdrop.getVisibleState());
+			});
+
+			xit('should queue operations', function() {
 				var backdrop = new MbBackdrop();
 
 				assert.equal(0, backdrop.queue.length);
